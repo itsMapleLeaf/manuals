7ebf9ad28582796b8cf35c53745d341035f0c788
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-20 20:58:56 -0500

    scripts/pull: auto-stash + fancy logs

--- scripts/pull/__main__.py
+++ scripts/pull/__main__.py
@@ -1,18 +1,34 @@
 import subprocess
+from git import Repo
 
 from ..lib.manuals import MANUALS
 from ..lib.paths import PROJECT_ROOT
 
 
-print("Updating Archipelago...")
+print("\n🏝️  Updating Archipelago...")
 subprocess.run(
     "git pull --tags --autostash origin main",
     cwd=PROJECT_ROOT / "Archipelago",
 )
 
+repo = Repo(PROJECT_ROOT)
+has_changes = repo.index.diff(None) != []
+
+# subtree calls fail if there are uncommitted changes, so ghetto auto-stash
+if has_changes:
+    print("\n⚠️  Found unsaved changes; stashing.")
+    print(
+        "⚠️  If you cancel the script before unstashing, run `git stash pop` yourself."
+    )
+    repo.git.stash("save")
+
 # update manuals
 for manual in MANUALS.values():
-    print(f"Updating {manual.name}...")
+    print(f"\n⚙️  Updating {manual.name}...")
     subprocess.run(f"git subtree pull --prefix {manual.root} manual main")
 
-print("Done")
+if has_changes:
+    print("\n🗄️  Restoring changes...")
+    repo.git.stash("pop")
+
+print("\n✅  Done")
