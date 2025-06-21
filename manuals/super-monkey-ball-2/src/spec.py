024c263529803416300fd9de2a0535fc66441a02
Darius <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-21 05:26:49 -0500

    smb2: use data classes
    i edited this on my phone lol hope this doesn't break

--- manuals/super-monkey-ball-2/src/spec.py
+++ manuals/super-monkey-ball-2/src/spec.py
@@ -1,174 +1,99 @@
 import json
+from dataclasses import dataclass
 
 from .world_spec import WorldSpec, requires
 
+@dataclass
+class LevelWorld:
+    name: str
+    levels: list[str]
 
 class SuperMonkeyBall2WorldSpec(WorldSpec):
     worlds = [
-        {
-            "name": "Jungle Island",
-            "levels": [
-                "Simple",
-                "Hollow",
-                "Bumpy",
-                "Switches",
-                "Conveyers",
-                "Floaters",
-                "Slopes",
-                "Sliders",
-                "Spinning Top",
-                "Curve Bridge",
+        LevelWorld(
+            name="Jungle Island",
+            levels=[
+                "Simple", "Hollow", "Bumpy", "Switches", "Conveyers",
+                "Floaters", "Slopes", "Sliders", "Spinning Top", "Curve Bridge",
             ],
-        },
-        {
-            "name": "Volcanic Magma",
-            "levels": [
-                "Banks",
-                "Eaten Floor",
-                "Hoppers",
-                "Coaster",
-                "Bumpy Check",
-                "Swell",
-                "Gravity Slider",
-                "Inchworms",
-                "Totalitarianism",
-                "Alternative",
+        ),
+        LevelWorld(
+            name="Volcanic Magma",
+            levels=[
+                "Banks", "Eaten Floor", "Hoppers", "Coaster", "Bumpy Check",
+                "Swell", "Gravity Slider", "Inchworms", "Totalitarianism", "Alternative",
             ],
-        },
-        {
-            "name": "Under the Ocean",
-            "levels": [
-                "Organic Form",
-                "Reversible Gear",
-                "Stepping Stones",
-                "Dribbles",
-                "U.R.L.",
-                "Mad Rings",
-                "Curvy Options",
-                "Twister",
-                "Downhill",
-                "Junction",
+        ),
+        LevelWorld(
+            name="Under the Ocean",
+            levels=[
+                "Organic Form", "Reversible Gear", "Stepping Stones", "Dribbles", "U.R.L.",
+                "Mad Rings", "Curvy Options", "Twister", "Downhill", "Junction",
             ],
-        },
-        {
-            "name": "Inside a Whale",
-            "levels": [
-                "Pro Skaters",
-                "Giant Comb",
-                "Beehive",
-                "Dynamic Maze",
-                "Triangle Holes",
-                "Launchers",
-                "Randomizer",
-                "Coin Slots",
-                "Seesaw Bridges",
-                "Arthropod",
+        ),
+        LevelWorld(
+            name="Inside a Whale",
+            levels=[
+                "Pro Skaters", "Giant Comb", "Beehive", "Dynamic Maze", "Triangle Holes",
+                "Launchers", "Randomizer", "Coin Slots", "Seesaw Bridges", "Arthropod",
             ],
-        },
-        {
-            "name": "Amusement Park",
-            "levels": [
-                "Wormhole",
-                "Free Fall",
-                "Melting Pot",
-                "Mad Shuffle",
-                "Bead Screen",
-                "Jump Machine",
-                "Zigzag Slope",
-                "Tower",
-                "Toggle",
-                "Fluctuation",
+        ),
+        LevelWorld(
+            name="Amusement Park",
+            levels=[
+                "Wormhole", "Free Fall", "Melting Pot", "Mad Shuffle", "Bead Screen",
+                "Jump Machine", "Zigzag Slope", "Tower", "Toggle", "Fluctuation",
             ],
-        },
-        {
-            "name": "Boiling Pot",
-            "levels": [
-                "Combination",
-                "Punched Seesaws",
-                "Opera",
-                "Brandished",
-                "Tiers",
-                "Cliffs",
-                "Narrow Peaks",
-                "Detour",
-                "Switch Inferno",
-                "Folders",
+        ),
+        LevelWorld(
+            name="Boiling Pot",
+            levels=[
+                "Combination", "Punched Seesaws", "Opera", "Brandished", "Tiers",
+                "Cliffs", "Narrow Peaks", "Detour", "Switch Inferno", "Folders",
             ],
-        },
-        {
-            "name": "Bubbly Washing Machine",
-            "levels": [
-                "Spiral Bridge",
-                "Wavy Option",
-                "Obstacle",
-                "Domino",
-                "Sieve",
-                "Flock",
-                "Double Spiral",
-                "Hierarchy",
-                "8 Bracelets",
-                "Quick Turn",
+        ),
+        LevelWorld(
+            name="Bubbly Washing Machine",
+            levels=[
+                "Spiral Bridge", "Wavy Option", "Obstacle", "Domino", "Sieve",
+                "Flock", "Double Spiral", "Hierarchy", "8 Bracelets", "Quick Turn",
             ],
-        },
-        {
-            "name": "Clock Tower Factory",
-            "levels": [
-                "Pistons",
-                "Soft Cream",
-                "Momentum",
-                "Entangled Path",
-                "Totters",
-                "Vortex",
-                "Warp",
-                "Trampolines",
-                "Swing Shaft",
-                "Linear Seesaws",
+        ),
+        LevelWorld(
+            name="Clock Tower Factory",
+            levels=[
+                "Pistons", "Soft Cream", "Momentum", "Entangled Path", "Totters",
+                "Vortex", "Warp", "Trampolines", "Swing Shaft", "Linear Seesaws",
             ],
-        },
-        {
-            "name": "Space Colony",
-            "levels": [
-                "Serial Jump",
-                "Cross Floors",
-                "Spinning Saw",
-                "Chipped Pipes",
-                "Flat Maze",
-                "Guillotine",
-                "Cork Screw",
-                "Orbiters",
-                "Twin Basin",
-                "Air Hockey",
+        ),
+        LevelWorld(
+            name="Space Colony",
+            levels=[
+                "Serial Jump", "Cross Floors", "Spinning Saw", "Chipped Pipes", "Flat Maze",
+                "Guillotine", "Cork Screw", "Orbiters", "Twin Basin", "Air Hockey",
             ],
-        },
+        ),
     ]
 
-    victory_world = {
-        "name": "Dr. Bad-Boon's Base",
-        "levels": [
-            "Training",
-            "Gimmick",
-            "Mountain",
-            "Disorder",
-            "3D Maze",
-            "Labyrinth",
-            "Postmodern",
-            "Revolution",
-            "Invisible",
-            "Created By",
+    victory_world = LevelWorld(
+        name="Dr. Bad-Boon's Base",
+        levels=[
+            "Training", "Gimmick", "Mountain", "Disorder", "3D Maze",
+            "Labyrinth", "Postmodern", "Revolution", "Invisible", "Created By",
         ],
-    }
+    )
 
     victory_level_required_banana_count = 5
 
     def __init__(self) -> None:
         for world_index, world in enumerate(self.worlds):
             world_item = self.item(
-                name=f"{world['name']} (World {world_index + 1})",
+                name=f"{world.name} (World {world_index + 1})",
                 category="Worlds",
                 progression=True,
             )
 
-            for level_index, level in enumerate(world["levels"]):
+            for level_index, level in enumerate(world.levels):
                 self.location(
                     name=f"{level_index + 1}. {level} (World {world_index + 1})",
                     category=f"World {world_index + 1}",
@@ -180,16 +105,16 @@ class SuperMonkeyBall2WorldSpec(WorldSpec):
             category="Bananas",
             count=int(
                 self.victory_level_required_banana_count
-                * len(self.victory_world["levels"])
+                * len(self.victory_world.levels)
                 * 1.5
             ),
             progression=True,
         )
 
-        for level_index, level in enumerate(self.victory_world["levels"]):
+        for level_index, level in enumerate(self.victory_world.levels):
             self.location(
-                name=f"{level_index + 1}. {level} ({self.victory_world['name']})",
-                category=self.victory_world["name"],
+                name=f"{level_index + 1}. {level} ({self.victory_world.name})",
+                category=self.victory_world.name,
                 requires=requires.item(
                     banana_item,
                     self.victory_level_required_banana_count * (level_index + 1),
@@ -202,8 +127,7 @@ class SuperMonkeyBall2WorldSpec(WorldSpec):
             category="Victory!",
             requires=requires.item(
                 banana_item,
-                self.victory_level_required_banana_count
-                * len(self.victory_world["levels"]),
+                self.victory_level_required_banana_count * len(self.victory_world.levels),
             ),
         )
 
