424a44b83a40bf904e88379c76353bf7ab0a3fbb
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-25 14:17:38 -0500

    preview: show categories

--- scripts/preview/__main__.py
+++ scripts/preview/__main__.py
@@ -9,6 +9,6 @@ manual_arg = sys.argv[1]
 manual = MANUALS[manual_arg]
 manual_data = manual.load_data()
 
-displayed_keys = {"item_table", "location_table", "option_table"}
-filtered_data = {k: v for k, v in manual_data.items() if k in displayed_keys}
+displayed_keys = {"item_table", "location_table", "option_table", "category_table"}
+filtered_data = {k: manual_data[k] for k in displayed_keys}
 print(json.dumps(filtered_data, indent="\t"))
