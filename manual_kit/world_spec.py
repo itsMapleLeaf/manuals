46592bf0a033956ef10506c76bb35ea4aa6fe7e1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-26 12:27:00 -0500

    manual_kit: new file for world spec stuff

---
+++ manual_kit/world_spec.py
@@ -0,0 +1,86 @@
+from typing import Optional, Unpack
+
+from .options import (
+    ChoiceOptionArgs,
+    ChoiceOptionData,
+    ChoiceOptionSpec,
+    OptionData,
+    RangeOptionArgs,
+    RangeOptionData,
+    RangeOptionSpec,
+    ToggleOptionArgs,
+    ToggleOptionData,
+    ToggleOptionSpec,
+)
+from .location import LocationArgs, LocationData, Requirement, Requires
+from .item import ItemArgs, ItemData
+from .category import CategoryArgs, CategoryData
+
+requires = Requires()
+
+
+class WorldSpecTable[T]:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.items: dict[str, T] = {}
+
+    def add(self, name: str, item: T) -> T:
+        if name in self.items:
+            raise Exception(f"Item {name} already exists in {self.name}")
+
+        self.items[name] = item
+        return item
+
+
+class WorldSpec:
+    def __init__(self) -> None:
+        self.items: list[ItemData] = []
+        self.locations: list[LocationData] = []
+        self.categories: dict[str, CategoryData] = {}
+        self.user_options: dict[str, OptionData] = {}
+
+    @property
+    def item_count(self) -> int:
+        return sum(item.get("count", 0) for item in self.items)
+
+    def item(self, name: str, **kwargs: Unpack[ItemArgs]) -> ItemData:
+        item = ItemData(name=name, **kwargs)
+        self.items += [item]
+        return item
+
+    def location(
+        self,
+        name: str,
+        requires: Optional[str | Requirement] = None,
+        **kwargs: Unpack[LocationArgs],
+    ) -> LocationData:
+        location = LocationData(name=name, **kwargs)
+
+        if requires != None:
+            location["requires"] = str(requires)
+
+        self.locations += [location]
+        return location
+
+    def category(self, name: str, **kwargs: Unpack[CategoryArgs]) -> CategoryData:
+        category = CategoryData(name=name, **kwargs)
+        self.categories[name] = category
+        return category
+
+    def toggle_option(
+        self, name: str, **kwargs: Unpack[ToggleOptionArgs]
+    ) -> ToggleOptionSpec:
+        self.user_options[name] = ToggleOptionData(**kwargs, type="Toggle")
+        return ToggleOptionSpec(name, kwargs)
+
+    def range_option(
+        self, name: str, **kwargs: Unpack[RangeOptionArgs]
+    ) -> RangeOptionSpec:
+        self.user_options[name] = RangeOptionData(**kwargs, type="Range")
+        return RangeOptionSpec(name, kwargs)
+
+    def choice_option(
+        self, name: str, **kwargs: Unpack[ChoiceOptionArgs]
+    ) -> ChoiceOptionSpec:
+        self.user_options[name] = ChoiceOptionData(**kwargs, type="Choice")
+        return ChoiceOptionSpec(name, kwargs)
